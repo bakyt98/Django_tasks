f36751c0605e820dba1bd3bea672e2dcb1e8a9d8
Bakyt <isimovabakyt@gmail.com>
2020-06-17 17:28:35 +0600

    ADD: manager, serializers, views

--- project_test/core/urls.py
+++ project_test/core/urls.py
@@ -1,4 +1,15 @@
 from django.urls import path
+from .views import create_user, get_all_users, CarViewSet, CarViewSet3
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
+    path('create_user/', create_user),
+    path('get_users/', get_all_users),
 ]
+
+router = DefaultRouter()
+
+router.register('cars', CarViewSet, basename="cars")
+router.register('cars2', CarViewSet3, basename='cars2')
+
+urlpatterns += router.urls
